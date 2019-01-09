0a31d102ee8c8410f8a8b81280af1222369f6f85
AnandaAtikaH <nathenathe4@gmail.com>
2019-01-09 14:18:00 +0700

    first akun

---
+++ src/kasirrestaurant/KasirRestaurant.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kasirrestaurant;
+
+/**
+ *
+ * @author USER
+ */
+public class KasirRestaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         KasirForm ks = new KasirForm();
+        ks.setVisible(true);
+    }
+    
+}
